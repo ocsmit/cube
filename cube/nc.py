ee699a57bb72873eabdfb9762ccf7c8da20a363f
ocsmit <osmith2@ncsu.edu>
2022-04-20 11:51:39 -0400

    Start to set up tests

--- cube/nc.py
+++ cube/nc.py
@@ -9,3 +9,41 @@
     :copyright: (c) 2022 by Owen Smith.
     :license: GNU v3.0, see LICENSE for more details.
 """
+
+
+import xarray as xr
+import numpy as np
+from pathlib import Path
+import rioxarray
+from datetime import datetime
+import re
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
+
+def convert_to_xarr(
+    file_dir: Path,
+    date_expression: str = "",
+    file_ext: str = "tif",
+    band_names: list = [
+        "blue",
+        "green",
+        "red",
+        "nir08",
+        "swir16",
+        "swir22",
+        "ndvi",
+        "qa",
+    ],
+) -> xr.core.dataarray.DataArray:
+
+    files = list(file_dir.rglob(f"*.{file_ext}"))
+    dates = [datetime.strptime(i.stem.split(".")[-1], "%Y%m%d") for i in files]
+    dates_ind = sorted_enumerate(dates)
+    # Standard xarray convention seems to name all time variables as time, even just dates
+    time = xr.Variable("time", dates)
+    xr_arr = xr.concat(
+        [rioxarray.open_rasterio(f, masked=True) for f in files], dim=time
+    )
+    xr_arr = xr_arr.assign_coords(band=band_names)
+    return xr_arr.sortby("time")
---
+++ cube/utils.py
@@ -0,0 +1,111 @@
+# Standard library imports
+
+import json
+from pathlib import Path
+from datetime import datetime
+import multiprocessing as mp
+from typing import Dict, Any, Union
+import time
+
+# Third party imports
+import pandas as pd
+import geopandas as gpd
+from osgeo import gdal
+import xarray as xr
+import rioxarray
+from shapely.geometry import mapping
+from pystac_client import Client
+from pystac_client.exceptions import APIError
+
+import warnings
+
+
+try:
+    from rich import print
+except ImportError:
+    pass
+
+
+def parse_bandmap(sat, path: Path = Path("./bandmap.json")):
+    """TODO: Docstring for parse_bandmap.
+
+    Parameters
+    ----------
+    path : TODO
+
+    Returns
+    -------
+    TODO
+
+    """
+    if not isinstance(path, Path):
+        try:
+            path = Path(path)
+        except TypeError:
+            print("Invalid path type")
+
+    with open(path.resolve(), "r") as fp:
+        band_map = json.load(fp)
+    try:
+        sat_map = band_map.get(sat)
+        return sat_map
+    except ValueError:
+        print(f"{sat} not in available band map")
+        return None
+
+
+def inv_bandmap(band_map: Dict[str, str]) -> Dict[str, str]:
+    """Generate inverted band, common name dictonary
+
+    Parameters
+    ----------
+    band_map : dict
+        Band map for data product
+
+    Returns
+    -------
+    dict
+        Band map with inverted band name and common name key, value pair
+
+    Example
+    -------
+        bmap =  {"HLSS30.v2.0": {
+                "B01": "aerosal"}}
+        inv_bandmap(bmap)
+
+    """
+    inv_dict = {}
+    collections = list(band_map.keys())
+    for c in collections:
+        inv_dict[c] = {v: k for k, v in band_map.get(c).items()}
+    return inv_dict
+
+
+def generate_client(
+    catalog="LPCLOUD", url="https://cmr.earthdata.nasa.gov/stac/"
+) -> Client:
+    """Connect to nasa stac
+
+    Parameters
+    ----------
+    catalog : str
+        Catalog to ingest from
+    url : str
+        STAC URL
+
+    Returns
+    -------
+    Client
+        STAC endpoint
+
+    Notes
+    -----
+        Simply a wrapper for Client.open.
+        TODO: Add checking and indexing of NASA cmr catalogs
+
+    """
+    try:
+        return Client.open(f"{url}/{catalog}")
+    except APIError:
+        warnings.warn("STAC endpoint not found", RuntimeWarning)
+        return None
