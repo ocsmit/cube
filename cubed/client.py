88ec55c915952f684c47968c2cebd1f215b326eb
ocsmit <osmith2@ncsu.edu>
2022-04-20 17:10:08 -0400

    Client documentation

--- cubed/client.py
+++ cubed/client.py
@@ -5,18 +5,27 @@ from typing import Union, Any, Dict, Tuple
 from pathlib import Path
 import json
 
+# Type
+SingleGeom = Dict[str, Union[str, Tuple]]
+
 
 class CubeClient(Client):
+    """
+    Extends the pystac_client Client class to provide handling of geometry
+    passed as a geojson path or GeoPandas dataframe object
+    """
+
     def __init__(self, **kwargs: Any) -> None:
         super(Client, self).__init__(**kwargs)
 
     def search(self, **kwargs: Any) -> ItemSearch:
+        # Wrap parent ItemSearch with geometry check
         if "intersects" in list(kwargs.keys()):
             geom = kwargs.get("intersects")
             kwargs["intersects"] = self.__check_intersects(geom)
         return super(CubeClient, self).search(**kwargs)
 
-    def __check_intersects(self, geom: Union[) -> Dict[str, Union[str, Tuple]]:
+    def __check_intersects(self, geom) -> SingleGeom:
         # Check if geom is geopandas dataframe object
         if isinstance(geom, gpd.GeoDataFrame):
             geo_json = geom.__geo_interface__
@@ -27,5 +36,5 @@ class CubeClient(Client):
                 geo_json = json.load(fp)
             return self.__get_subgeom(geo_json)
 
-    def __get_subgeom(self, geom_json) -> Dict[str, Union[str, Tuple]]:
+    def __get_subgeom(self, geom_json) -> SingleGeom:
         return geom_json["features"][0]["geometry"]
