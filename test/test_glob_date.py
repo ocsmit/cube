d09e39acb6bc6767abc5eef8ec3e9dda1f9ab0e5
ocsmit <osmith2@ncsu.edu>
2022-06-07 14:51:08 -0400

    Add more glob_date tests

--- test/test_glob_date.py
+++ test/test_glob_date.py
@@ -1,10 +1,23 @@
 import pytest
 from cubed.date_glob import DateGlob
 
-date_str = "%Y-%m-%d"
-correct_pattern = "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]"
 
-
-def test_glob_date():
+def test_glob_date1():
+    date_str = "%Y-%m-%d"
+    correct_pattern = "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]"
     pattern = DateGlob(date_str)
     assert pattern.pattern == correct_pattern
+
+
+def test_glob_date2():
+    date_str = "%Y%m%d"
+    correct_pattern = "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]"
+    pattern = DateGlob(date_str)
+    assert pattern.pattern == correct_pattern
+
+
+def test_glob_date_fail():
+    date_str = "%Y-%m-%d"
+    correct_pattern = "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]"
+    pattern = DateGlob(date_str)
+    assert pattern.pattern != correct_pattern
